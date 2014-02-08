8b866dd3cb815278f57a1bb26049abb0e8598c4b
steich <steich@gmail.com>
2014-02-08 13:46:43 -0600

    changed image locations

--- nasa_turk/settings.py
+++ nasa_turk/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 STATIC_URL = BASE_DIR + '/static/'
-
+MEDIA_ROOT = BASE_DIR + '/media/'
+MEDIA_URL = 'http://127.0.0.1:8000/media/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
